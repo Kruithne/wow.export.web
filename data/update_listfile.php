850bf4a039711fcaaaf1f81bc30d6cb43eafebf3
Kruithne <kruithne@gmail.com>
2022-03-12 23:12:08 +0000

    Download listfile/tact to temporary file to prevent concurrency issues during update

--- data/update_listfile.php
+++ data/update_listfile.php
@@ -1,7 +1,8 @@
 <?php
 	if (php_sapi_name() == "cli")
 	{
-		file_put_contents(__DIR__ . "/listfile/master", fopen("https://raw.githubusercontent.com/wowdev/wow-listfile/master/community-listfile.csv", 'r'));
+		file_put_contents(__DIR__ . "/listfile/master.tmp", fopen("https://raw.githubusercontent.com/wowdev/wow-listfile/master/community-listfile.csv", 'r'));
+		rename(__DIR__ . "/listfile/master.tmp", __DIR__ . "/listfile/master");
 	}
 	else
 	{
--- data/update_tact.php
+++ data/update_tact.php
@@ -1,7 +1,8 @@
 <?php
 	if (php_sapi_name() == "cli")
 	{
-		file_put_contents(__DIR__ . "/tact/wow", fopen("https://raw.githubusercontent.com/wowdev/TACTKeys/master/WoW.txt", 'r'));
+		file_put_contents(__DIR__ . "/tact/wow.tmp", fopen("https://raw.githubusercontent.com/wowdev/TACTKeys/master/WoW.txt", 'r'));
+		rename(__DIR__ . "/tact/wow.tmp", __DIR__ . "/tact/wow");
 	}
 	else
 	{
