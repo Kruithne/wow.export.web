5eab6db949258a61fc2e6c18fd0ee54958bab7c8
Kruithne <kruithne@gmail.com>
2025-06-10 20:49:34 +0100

    use static directory path relative to hosting server process

--- module.ts
+++ module.ts
@@ -4,7 +4,7 @@ import path from 'node:path';
 type SpooderServer = ReturnType<typeof serve>;
 
 export function init(server: SpooderServer) {
-	server.dir('/wow.export', './public', async (file_path, file, stat, _request) => {
+	server.dir('/wow.export', './wow.export/public', async (file_path, file, stat, _request) => {
 		// ignore hidden files
 		if (path.basename(file_path).startsWith('.'))
 			return 404; // Not Found
