df1be13e2e5c575c12929fd441e909f0a127e5a3
Kruithne <kruithne@gmail.com>
2025-06-10 20:45:24 +0100

    create spooder module to attach into kruithne.net

---
+++ module.ts
@@ -0,0 +1,18 @@
+import { serve } from 'spooder';
+import path from 'node:path';
+
+type SpooderServer = ReturnType<typeof serve>;
+
+export function init(server: SpooderServer) {
+	server.dir('/wow.export', './public', async (file_path, file, stat, _request) => {
+		// ignore hidden files
+		if (path.basename(file_path).startsWith('.'))
+			return 404; // Not Found
+		
+		// ignore directories
+		if (stat.isDirectory())
+			return 401; // Unauthorized
+		
+		return file;
+	});
+}
