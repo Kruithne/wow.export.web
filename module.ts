83fe145c5adcddc32d1bf2f918664476b048aa04
Kruithne <kruithne@gmail.com>
2025-06-10 22:33:03 +0100

    port over old php logic to new server module

--- module.ts
+++ module.ts
@@ -1,7 +1,58 @@
-import { serve } from 'spooder';
+import { serve, caution } from 'spooder';
 import crypto from 'node:crypto';
+import os from 'node:os';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const UPDATE_TIMER = 24 * 60 * 60 * 1000; // 24 hours
 
 type SpooderServer = ReturnType<typeof serve>;
+
+async function download_and_store(url: string, target_dir: string, target_filename: string, name: string) {
+	const target_file = path.join(target_dir, target_filename);
+	let temp_file: string | null = null;
+	
+	try {
+		const response = await fetch(url);
+		if (!response.ok)
+			throw new Error(`Failed to fetch ${name}: ${response.status} ${response.statusText}`);
+		
+		temp_file = path.join(os.tmpdir(), `${name}_${crypto.randomUUID()}.tmp`);
+		
+		await Bun.write(temp_file, response);
+		await fs.mkdir(target_dir, { recursive: true });
+		await fs.rename(temp_file, target_file);
+
+		temp_file = null;
+	} catch (error) {
+		caution(`Failed to update ${name}`, [error instanceof Error ? error.message : String(error)]);
+		throw error;
+	} finally {
+		if (temp_file)
+			await fs.unlink(temp_file);
+	}
+}
+
+async function update_data_files() {
+	await update_listfile();
+	await update_tact();
+
+	setTimeout(update_data_files, UPDATE_TIMER);
+}
+
+async function update_listfile() {
+	const url = 'https://raw.githubusercontent.com/wowdev/wow-listfile/master/community-listfile.csv';
+	const target_dir = './wow.export/data/listfile';
+	
+	await download_and_store(url, target_dir, 'master', 'listfile');
+}
+
+async function update_tact() {
+	const url = 'https://raw.githubusercontent.com/wowdev/TACTKeys/master/WoW.txt';
+	const target_dir = './wow.export/data/tact';
+	
+	await download_and_store(url, target_dir, 'wow', 'tact');
+}
 
 let index: string|null = null;
 let index_hash: string|null = null;
@@ -25,5 +76,21 @@ export function init(server: SpooderServer) {
 		return new Response(index, { status: 200, headers });
 	});
 
+	server.route('/wow.export/data/dbd', async (req, url) => {
+		const def = url.searchParams.get('def');
+		if (def === null)
+			return 404;
+
+		const dbd = await fetch(`https://raw.githubusercontent.com/wowdev/WoWDBDefs/master/definitions/${def}.dbd`);
+		return new Response(dbd.body, {
+			status: dbd.status,
+			headers: dbd.headers 
+		});
+	});
+
+	server.dir('/wow.export/data', './wow.export/data');
 	server.dir('/wow.export/static', './wow.export/static');
+	server.dir('/wow.export/update', './wow.export/update');
+
+	update_data_files();
 }
